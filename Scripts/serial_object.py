823469fa203f631b9fd724868924bd6289e4be90
andersonvnicius <andersonvnicius@gmail.com>
2021-06-09 00:28:03 -0300

    added list_serial_port function

--- Scripts/serial_object.py
+++ Scripts/serial_object.py
@@ -3,6 +3,41 @@ object for using with serial devices
 """
 
 
+def get_serial_ports():
+    """
+    lists serial port names
+    source: https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of the serial ports available on the system
+    """
+    import sys
+    import glob
+    import serial
+
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    port_list = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            port_list.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return port_list
+
+
 class SerialDevice:
     """
     Creates a serial connection device object
