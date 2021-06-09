5c1dc878f708843211cf689806c1812b6b514404
andersonvnicius <andersonvnicius@gmail.com>
2021-06-09 01:57:08 -0300

    added live plot and serial monitor functionality

--- Scripts/serial_object.py
+++ Scripts/serial_object.py
@@ -44,8 +44,9 @@ class SerialDevice:
     """
     from serial import Serial
 
-    def __init__(self, port, baudrate):
+    def __init__(self, port, baudrate, delay_time):
         self.serial_object = self.Serial(port=port, baudrate=baudrate, timeout=1)
+        self.delay_time = float(delay_time)*1e-3
 
     def read_line(self):
         """returns the last line of data sent by the device"""
@@ -62,3 +63,72 @@ class SerialDevice:
             data.append(self.read_line())
             current_sample += 1
         return data
+
+    def plot_data(self, n_of_samples=False):
+        """
+        read and plot any numerical serial received data as long it is in the following format:
+        b'<data_1> <data_2> <data_3> ... <data_n>/r/n'
+
+        TBD:
+        - write about:
+          -- input variables:
+           - n_of_samples
+          -- outputs:
+           - live plot to the user
+        """
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        self.serial_object.close()
+        self.serial_object.open()
+        self.serial_object.flushInput()
+        self.serial_object.readline()
+
+        data = self.read_line().decode().rstrip('\r\n').split(' ')
+
+        # LIVE-PLOT SETUP
+        plt.ion()
+        n_of_plots = len(data)
+        fig, axs = plt.subplots(n_of_plots)
+        fig.suptitle('Sensor readings')
+
+        # empty variables
+        data = np.zeros((1, n_of_plots))
+
+        # indexes
+        error_qnt = 0
+        n_of_iterations = 0
+        current_sample = 0
+
+        while True:
+            plt.cla()
+            read_float = []
+            n_of_iterations += 1
+            print(current_sample)
+
+            try:
+                self.serial_object.flushInput()
+                read = self.serial_object.readline().decode().rstrip('\r\n').split(' ')
+
+                for value in read:
+                    read_float.append(float(value))
+
+                data = np.append(data, [read_float], axis=0)
+
+                for j in range(0, n_of_plots):
+                    axs[j].cla()
+                    axs[j].plot(data[:, j][-5:], ('C' + str(j)))
+
+                plt.pause(self.delay_time)
+
+                if isinstance(n_of_samples, bool):
+                    pass
+
+                else:
+                    if current_sample > n_of_samples - 2:
+                        break
+                    current_sample += 1
+
+            except IndexError:
+                error_qnt += 1
+                print('Index Error rate', round(error_qnt / n_of_iterations, 2))
